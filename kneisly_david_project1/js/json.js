794811b337b4a0eabfbbf2537d25b0942bcf3330
Tyler Kneisly <tkneisly@fullsail.edu>
2012-05-27 16:02:26 -0400

    Added VFW Week 4 project files to MIU Week 1 project.

---
+++ kneisly_david_project1/js/json.js
@@ -0,0 +1,40 @@
+// Week 4, Project 4
+// David Tyler Kneisly
+// VFW 1205
+// Book Tracker (JSON.js)
+
+var json = {
+	"book1": {
+		"groups": ["Group:","Tablet"],
+		"titles": ["Title:", "Once Upon a Mattress"],
+		"authors": ["Author:", "Thompson, Jay"],
+		"readpages": ["Pages:", "1959"],
+		"datefinished": ["Date Finished:", "2008-06-20"],
+		"rating": ["Rating:", "8"],
+		"category": ["Genre:", "Fantasy"],
+		"favs": ["Favorite:", "Yes"],
+		"note": ["Notes:", "She certainly liked almost both his parents."]
+	},
+	"book2": {
+		"groups": ["Group:","Book"],
+		"titles": ["Title:", "Clementine's Decent"],
+		"authors": ["Author:", "Darling, Obi"],
+		"readpages": ["Pages:", "1849"],
+		"datefinished": ["Date Finished:", "2012-03-22"],
+		"rating": ["Rating:", "6"],
+		"category": ["Genre:", "Classic"],
+		"favs": ["Favorite:", "No"],
+		"note": ["Notes:", "Apparently, beware of open-toed sandals."]
+	},
+	"book3": {
+		"groups": ["Group:", "EReader"],
+		"title": ["Title", "Thinking Fourth Dimensionally"],
+		"authors": ["Author:", "Brown, Emmett"],
+		"readpages": ["Pages:", "121"],
+		"datefinished": ["Date Finished:", "1985-11-05"],
+		"rating": ["Rating:", "10"],
+		"category": ["Genre:", "Science-Fiction"],
+		"favs": ["Favorite:", "Yes"],
+		"note": ["Notes:", "Where we're going, we don't need roads."]
+	}
+}

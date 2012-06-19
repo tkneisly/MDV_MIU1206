ab1af214ec9baf071dc4b3a6a3a2e57d6f66b869
Tyler Kneisly <tkneisly@fullsail.edu>
2012-06-19 13:40:34 -0400

    Update to all Week 3 Project files.  Includes corrections to errors in code and file structure caused by Coda 2 Application bug (known issue being addressed by devs).

--- kneisly_david_project3/Gold/js/main.js
+++ kneisly_david_project3/Gold/js/main.js
@@ -1,4 +1,4 @@
-// Week 3, Project 3
+// Week 4, Project 4
 // David Tyler Kneisly
 // MUI 1206
 // Book Tracker
@@ -9,36 +9,6 @@
 
 var parseBookForm = function(itm){
 	console.log(localStorage);
-	/*
-	if(!key) {
-		var id = Math.floor(Math.random()*100000001);
-	} else {
-		// Set the id to the existing key if the item is being edited
-		// From here, key is passed to the validate function, then into storeData
-		id = key;
-	}
-	for (var i=0; i < data.length; i++) {
-			if (radios[i].checked) {
-				genreValue = radios[i].value;
-			}
-		}
-	var item = {};
-	item.groups = ['Group:', ge('groups').value];
-	item.titles = ['Title:', ge('booktitle').value];
-	item.authors = ['Author:', ge('author').value];
-	item.readpages = ['Pages:', ge('pages').value];
-	item.datefinished = ['Date Finished:', ge('date').value];
-	item.rating = ['Rating:', ge('rating').value];
-	item.category = ['Genre:', genreValue];
-	item.favs = ['Favorite:', favoriteValue];
-	item.note = ['Notes:', ge('notes').value];
-	localStorage.setItem(id, JSON.stringify(item));
-	// Data is saved into Local Storage; Using 'Stringify' to convert the object into a string
-	alert('Book is saved!');
-	window.location.reload();
-	scrollTo(0,1);
-	return false;
-	*/
 };
 
 
@@ -47,25 +17,8 @@ $(document).bind('pageinit', function(){
 
 	var
 	bookForm = $('#addbookform'),
-	errorslink = $('#errorslink'),
-	item = {}
+	errorslink = $('#errorslink')
 	;
-	
-	$.fn.serializeObject = function(){
-	    var itm = item;
-	    var a = this.serializeArray();
-	    $.each(a, function() {
-	        if (itm[this.name] !== undefined) {
-	            if (!itm[this.name].push) {
-	                itm[this.name] = [itm[this.name]];
-	            }
-	            itm[this.name].push(this.value || '');
-	        } else {
-	            itm[this.name] = this.value || '';
-	        }
-	    });
-	    return itm;
-	};
 	
 	bookForm.validate({
 		// Fires if invalid values are detected in required fields
@@ -81,13 +34,12 @@ $(document).bind('pageinit', function(){
 			$("#geterrors ul").html(output);
 			console.log(validator.submitted);
 		},
-		//focusInvalid:false,
+
 		// Fires when form is submitted and there are no validation errors
-		submitHandler: function(itm) {
-			var id = Math.floor(Math.random()*100000001);
-			var outForm = $('#result').text(JSON.stringify($('bookForm').serializeObject()));
-			localStorage.setItem(id, itm);
-			return false;
+		submitHandler: function() {
+			var data = bookForm.serializeArray();
+			parseBookForm(data);
+			//storeData();
 		}
 	})
 });
